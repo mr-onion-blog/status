aced0ab0f9649cd7b703a4ba88e11465f0e25fba
Mr_Onion <142563755+SuperUseryjh@users.noreply.github.com>
2025-02-16 20:59:48 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/SuperUseryjh/status",
+  home: "https://mr-onion-blog.fun",
+  email: "mailto:onion0408@outlook.com",
 };
 </script>
 
